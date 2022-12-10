51c6460538f5b2003ef6ba4c3adefddce8cfbea3
juniorT34 <117579160+juniorT34@users.noreply.github.com>
2022-12-10 19:18:49 +0200

    update js file

--- script.js
+++ script.js
@@ -3,27 +3,41 @@ function getComputerChoice(){
     Choices = ["rock","paper","scissors"]
     return Choices[Math.floor(Math.random() * 3)]
 }
-
-//getComputerChoice()
+let score1 = 0
+let score2 = 0
 //checking user's choice with computer's
 function playRound(playerSelection, computerSelection){
-    
     playerSelection = playerSelection.toLowerCase()
     if (playerSelection === computerSelection){
         console.log("it's a draw")
     }else if((computerSelection === "rock" && playerSelection === "scissors") || (computerSelection === "paper" && playerSelection === "rock") || (computerSelection === "scissors" && playerSelection === "paper")){
         console.log("You lose " + computerSelection + " beats " + playerSelection)
+        score1++
     }else if((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper") ){
         console.log("You win " + playerSelection + " beats " + computerSelection)
+        score2++
     }else{
         console.log("Please enter a correct input")
     }
     
 }
 
-const player = "rockyou"
-console.log(player)
-let computer = getComputerChoice()
-console.log(computer)
-//play five times
-playRound(player,computer)
+//play five times 
+function game(){
+    for(let i = 0 ; i >5; i++){
+        const player = "rock"
+        let computer = getComputerChoice()
+        playRound(player,computer)
+    }
+    //score
+    if(score1 > score2){
+        console.log("The winner is the computer with a score of " + score1 + " over " + score2)
+    }else if(score2 > score1){
+        console.log("The winner is the player with a score of " + score2 + " over " + score1)
+    }else{
+        console.log("it's a draw, no winner")
+    }
+}
+//console.log(player)
+//console.log(computer)
+game()
